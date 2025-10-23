0f17146e4cf4707c10c8f68cbeb28a81b3218e2d
Victor Bechet <166110416+Aeroloutre@users.noreply.github.com>
2025-10-23 10:27:08 +0000

    backend routes handling

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,32 @@
-const express = import ('express')
-const app = express()
-const port = 3000
+import dotenv from 'dotenv'
+import express from 'express';
+import pgPromise from 'pg-promise'
 
+dotenv.config({ path: '../.env' })
+
+const app = express()
+const port = 4000
+
+const pgp = pgPromise();
+const db = pgp(process.env.DB_LINK);
+
+//Routes
 app.get('/', (req, res) => {
-  res.send('THis is my express server !')
+  res.send('This is my express server !')
+})
+
+app.get('/status', (req, res) => {
+  res.send('OK ! Le server est UP :D')
+})
+
+app.get('/items', (req, res) => {
+  res.send((readAllItems()))
 })
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
 })
+
+const readAllItems = async () => {
+  await db.any('SELECT * FROM items')
+}
