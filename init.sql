8ac287c8a8921a5a317cc06d8cc8181860fe6950
Victor Bechet <vbechet@solem.fr>
2025-10-23 14:02:35 +0200

    init for DB and update on docker compose for front and backend

---
+++ init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS items (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    size INT,
+);
+
+INSERT INTO items (name, size) VALUES
+('mon premier item', 60),
+('mon deuxieme item', 154),
+('mon troisieme item', 987);
